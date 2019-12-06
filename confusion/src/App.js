c22fb10f12513b7aeee5f911de8bd9d18e88b98e
Ian <ihafkenschiel@gmail.com>
2019-12-05 20:33:07 -0800

    Adding reacstrap features

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
+      <Navbar dark color="primary" expand="md">
+        <NavbarBrand target="_blank" href="https://www.coursera.org/learn/front-end-react/supplement/HXLrI/introduction-to-react-additional-resources">Ristorante Con Fusion</NavbarBrand>
+        <Nav navbar>
+          <NavItem>
+            <NavLink target="_blank" href="https://reactstrap.github.io/components/navbar/">Reactstrap</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>Empty Link</NavLink>
+          </NavItem>
+        </Nav>
       </Navbar>
     </div>
   );

5b7bdaf2bfc09b4aa06a3173ef11d40e37d1ba43
Ian <ihafkenschiel@gmail.com>
2019-12-22 15:03:02 -0800

    Fixing compile bugs

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {

0cd780266d5dca5966b3abeffe67422cbce9f9b5
Ian <ihafkenschiel@gmail.com>
2019-12-23 21:15:19 -0800

    Single page application part 1 working

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -6,6 +6,9 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 //import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,7 +16,10 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -25,7 +31,11 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home
+          dish={ this.state.dishes.filter( (dish) => dish.featured )[0] }
+          promotion={ this.state.promotions.filter( (promo) => promo.featured )[0] }
+          leader={ this.state.leaders.filter( (leader) => leader.featured )[0] }
+         />
       );
     }
 

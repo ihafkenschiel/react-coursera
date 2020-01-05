f8b876bc46c59346b3d5c91880d6f8d99abdc05e
Ian <ihafkenschiel@gmail.com>
2020-01-05 13:54:12 -0800

    Assignment 3 task 2 - added react redux form elements

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -11,9 +11,16 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-  Form
+  Form,
+  Row,
+  Label,
+  Col
 } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Link} from 'react-router-dom';
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
 
 function RenderDish({dish}) {
   return (<div>
@@ -71,11 +78,66 @@ const CommentForm = (props) => {
         <span className="fa fa-pencil fa-lg"></span> Submit Comment
       </Button>
       <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader>Login</ModalHeader>
+        <ModalHeader>Submit Comment</ModalHeader>
         <ModalBody>
-          <Form>
-            <Button type="submit" value="submit" color="primary">Login</Button>
-          </Form>
+          <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Row className="form-group">
+              <Label htmlFor="rating" md={2}>Rating</Label>
+              <Col md={10}>
+                <Control.select model=".rating"
+                  id="rating"
+                  name="rating"
+                  className="form-control"
+                 >
+                 <option>1</option>
+                 <option>2</option>
+                 <option>3</option>
+                 <option>4</option>
+                 <option>5</option>
+               </Control.select>
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Label htmlFor="author" md={2}>Your Name</Label>
+              <Col md={10}>
+                <Control.text model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    minLength: minLength(3), maxLength: maxLength(15)
+                  }}
+                 />
+               <Errors
+                 className="text-danger"
+                 model=".author"
+                 show="touched"
+                 messages={{
+                   minLength: 'Must be greater than 2 characters',
+                   maxLength: 'Must be 15 characters or less'
+                 }}
+                 />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Label htmlFor="comment" md={2}>Comment</Label>
+              <Col md={10}>
+                <Control.textarea model=".comment"
+                  id="comment"
+                  name="comment"
+                  rows="6"
+                  className="form-control" />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col md={{size: 10, offset: 2}}>
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </LocalForm>
         </ModalBody>
       </Modal>
     </div>

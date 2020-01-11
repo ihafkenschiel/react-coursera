804eb185d132e2e9247ee805e7262e433240edfe
Ian <ihafkenschiel@gmail.com>
2020-01-11 11:33:33 -0800

    Forgot to add combineReducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';

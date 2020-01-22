a9972b39a25acaee80b387e290ee28e95d9f8925
Ian <ihafkenschiel@gmail.com>
2020-01-22 17:21:06 -0300

    map the form into the store so that the state of the form will be persisted in the store

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import DishDetail from './DishdetailComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -21,7 +22,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch( addComment(dishId, rating, author, comment) ),
-  fetchDishes: () => { dispatch( fetchDishes() ) }
+  fetchDishes: () => { dispatch( fetchDishes() ) },
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -64,7 +66,7 @@ class Main extends Component {
         <Route exact="exact" path="/menu" component={() => <Menu dishes={this.props.dishes}/>} />
         <Route path='/menu/:dishId' component={DishWithId}/>
         <Route exact="exact" path="/aboutus" component={() => <About leaders={this.props.leaders}/>} />
-        <Route exact="exact" path="/contactus" component={Contact}/>
+        <Route exact path='/contactus' component={ () => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
         <Redirect to="/home"/>
       </Switch>
       <Footer/>

9f67a9fa9371b682817d400ab53305a0636076d0
Ian <ihafkenschiel@gmail.com>
2020-01-26 22:46:40 -0300

    Fetch dishes comments and promos from json-server

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import {
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -25,7 +26,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 function RenderDish({dish}) {
   return (<div>
     <Card>
-      <CardImg width="100%" src={dish.image} alt={dish.name}/>
+      <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
       <CardBody>
         <CardTitle>{dish.name}</CardTitle>
         <CardText>{dish.description}</CardText>

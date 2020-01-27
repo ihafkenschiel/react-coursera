e7ffcef691e765716aef9e073f21ae5851c2e573
Ian <ihafkenschiel@gmail.com>
2020-01-26 23:30:00 -0300

    Fetch error handling

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -49,10 +49,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return (
-          <Home
+        <Home
               dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
               dishesLoading={this.props.dishes.isLoading}
-              dishErrMess={this.props.dishes.errMess}
+              dishesErrMess={this.props.dishes.errMess}
               promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
               promoLoading={this.props.promotions.isLoading}
               promoErrMess={this.props.promotions.errMess}
@@ -64,12 +64,12 @@ class Main extends Component {
     const DishWithId = ({match}) => {
       return (
         <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-          isLoading={this.props.dishes.isLoading}
-          errMess={this.props.dishes.errMess}
-          comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-          commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
-        />
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            commentsErrMess={this.props.comments.errMess}
+            addComment={this.props.addComment}
+          />
       );
     };
 

8998337e0a730ba4924780c2cc59c0cb9e8c03e8
Ian <ihafkenschiel@gmail.com>
2020-01-27 21:04:02 -0300

    Assignment 4 task 1 - update the Redux actions and the Home and About components to use the data from the server for rendering the leader information

--- confusion/src/redux/promotions.js
+++ confusion/src/redux/promotions.js
@@ -5,7 +5,7 @@ export const Promotions = (state  = { isLoading: true,
                                         promotions:[]}, action) => {
     switch (action.type) {
         case ActionTypes.ADD_PROMOS:
-        return {...state, isLoading: false, errMess: null, promotions: action.payload};
+          return {...state, isLoading: false, errMess: null, promotions: action.payload};
 
         case ActionTypes.PROMOS_LOADING:
             return {...state, isLoading: true, errMess: null, promotions: []}
